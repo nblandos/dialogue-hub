168563b5eee7c5e2e62ad6ee345455f58e80521b
Nicholas Blandos <nblandos@gmail.com>
2024-12-02 19:42:05 +0000

    More setup, formatters and testing (#3)
    * add linters and formatters: autopep8, flake8, eslint, prettier
    
    * fix typo in readme
    
    * add pytest

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ An accessible timeslot booking tool for the Dialogue Hub's [Cafe](https://dialog
 - Efe Tekin
 - Pratham Shah
 
-## How to Run Locally
+## Development
 
 ### Backend
 
@@ -46,6 +46,19 @@ export FLASK_ENV=development
 flask run
 ```
 
+#### To Run Tests
+
+```bash
+pytest
+```
+
+#### To Check Test Coverage
+
+```bash
+coverage run -m pytest
+coverage report
+```
+
 Backend runs on `http://localhost:5000`.
 
 ### Frontend
@@ -66,7 +79,7 @@ Frontend runs on `http://localhost:3000`.
 
 ### Docker
 
-If you want to the whole project using Docker, first you must have Docker and Docker Compose installed on your machine.
+If you want to run the whole project using Docker, first you must have Docker and Docker Compose installed on your machine.
 
 #### Build Docker Image
 
@@ -87,3 +100,10 @@ docker-compose down
 ```
 
 _Note_: you may need to start with `sudo` for the above commands if you get permission errors.
+
+### Linters and Formatters
+
+Backend uses autopep8 and flake8 for formatting and linting.
+Frontend uses ESLint and Prettier for formatting and linting.
+
+I recommend installing VSCode extensions and configuring them to format on save.
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,38 +1,47 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals';
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  { ignores: ['dist'] },
+  js.configs.recommended,
+  prettierConfig,
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
-    settings: { react: { version: '18.3' } },
     plugins: {
+      prettier,
       react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-]
+];
