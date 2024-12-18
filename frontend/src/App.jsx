7186183aeee8296e832a908c5377a20ae959e4df
nblandos <nblandos@gmail.com>
2024-12-18 18:45:15 +0000

    add menu/scheduling pages without content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/common/Header.jsx';
+import SchedulePage from './pages/booking/SchedulePage';
+import MenuPage from './pages/menu/MenuPage';
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="flex flex-col grow container mx-auto">
+      <Header />
+      <Routes>
+        <Route path="/" element={<SchedulePage />} />
+        <Route path="/menu" element={<MenuPage />} />
+      </Routes>
+    </div>
   );
 }
 
