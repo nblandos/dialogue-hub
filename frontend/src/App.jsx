b1e2ead2f393e9c763c5e3bf6b377959fa9592de
Nicholas Blandos <nblandos@gmail.com>
2024-12-18 18:49:27 +0000

    Create header component (#4)
    * add tailwind config + other config changes
    
    * create header component
    
    * add menu/scheduling pages without content
    
    * add some comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/common/Header.jsx';
+import SchedulePage from './pages/booking/SchedulePage';
+import MenuPage from './pages/menu/MenuPage';
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="flex flex-col grow container mx-auto">
+      <Header />
+      <Routes>
+        {/* path to schedule timeslot page is / as I think it should be the default page */}
+        <Route path="/" element={<SchedulePage />} />
+        <Route path="/menu" element={<MenuPage />} />
+      </Routes>
+    </div>
   );
 }
 
