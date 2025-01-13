343e8018c2d9555931cecd4a63a923f6ac79a18a
nblandos <nblandos@gmail.com>
2025-01-13 12:38:27 +0000

    update model to include timeslots

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -5,19 +5,21 @@ from src.app import db
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), primary_key=True)
     full_name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
     created_at = db.Column(
         db.DateTime, default=lambda: datetime.now(timezone.utc))
 
-    bookings = db.relationship('Booking', backref='user', lazy=True)
+    bookings = db.relationship(
+        'Booking', backref='user', lazy=True, cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<User {self.email}>'
 
     def to_dict(self):
         return {
-            "id": self.id,
-            "full_name": self.full_name,
             "email": self.email,
+            "full_name": self.full_name,
             "created_at": self.created_at.isoformat(),
             "bookings": [booking.to_dict() for booking in self.bookings]
         }
@@ -25,6 +27,6 @@ class User(db.Model):
     @staticmethod
     def from_dict(data):
         return User(
-            full_name=data.get("full_name"),
-            email=data.get("email")
+            email=data.get("email"),
+            full_name=data.get("full_name")
         )
