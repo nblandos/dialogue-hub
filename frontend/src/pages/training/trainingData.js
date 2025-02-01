2cbe482e8ad347180b0a99622b1366061fe14791
Efe Tekin <-replace-all>
2025-02-01 21:52:52 +0000

    Added Training Video Page

---
+++ frontend/src/pages/training/trainingData.js
@@ -0,0 +1,56 @@
+const trainingMenu = [
+  {
+    name: 'Good morning',
+    video: 'https://www.youtube.com/embed/036crssUsHQ?enablejsapi=1',
+  },
+  {
+    name: 'How are you?',
+    video: 'https://www.youtube.com/embed/MfTXRJAoj9c?enablejsapi=1',
+  },
+  {
+    name: 'Good evening',
+    video: 'https://www.youtube.com/embed/MkKTsYKVTU0?enablejsapi=1',
+  },
+  {
+    name: 'Hello-Please-Thank You',
+    video: 'https://www.youtube.com/embed/uvuOS6ft6sI?enablejsapi=1',
+  },
+  {
+    name: 'Change my mind',
+    video: 'https://www.youtube.com/embed/uQYRejJA2Ss?enablejsapi=1',
+  },
+  {
+    name: 'Hello',
+    video: 'https://www.youtube.com/embed/cKxRKMfMz-Q?enablejsapi=1',
+  },
+  {
+    name: "It's a lovely day",
+    video: 'https://www.youtube.com/embed/rOCJBJITS7s?enablejsapi=1',
+  },
+  {
+    name: 'See you later',
+    video: 'https://www.youtube.com/embed/luJDUfQwzCw?enablejsapi=1',
+  },
+  {
+    name: "I'm fine",
+    video: 'https://www.youtube.com/embed/zJr2IOX3yO4?enablejsapi=1',
+  },
+  {
+    name: "I don't like it",
+    video: 'https://www.youtube.com/embed/B_0R2x48QmA?enablejsapi=1',
+  },
+  {
+    name: 'Do you like it?',
+    video: 'https://www.youtube.com/embed/NzXLtP5lpNo?enablejsapi=1',
+  },
+  {
+    name: 'Goodbye',
+    video: 'https://www.youtube.com/embed/rYUxSTPDDM0?enablejsapi=1',
+  },
+  {
+    name: 'Yes, I like it',
+    video: 'https://www.youtube.com/embed/rIR7jwDlPs8?enablejsapi=1',
+  },
+];
+
+export default trainingMenu;
---
+++ frontend/src/pages/training/trainingPage.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import trainingMenu from './trainingData';
+import MenuDiv from '../../components/menu/MenuDiv';
+
+const TrainingPage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortOption, setSortOption] = useState('asc');
+
+  const filteredMenu = trainingMenu
+    .filter((item) =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (sortOption === 'asc') {
+        return a.name.localeCompare(b.name);
+      }
+      if (sortOption === 'desc') {
+        return b.name.localeCompare(a.name);
+      }
+    });
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-6 pt-32">
+      <h1 className="mb-8 text-center text-3xl font-bold">Training Videos</h1>
+      <div className="mb-6 flex items-center justify-between">
+        <label htmlFor="search" className="sr-only">
+          Search Training Videos
+        </label>
+        <input
+          id="search"
+          type="text"
+          placeholder="Search..."
+          className="w-1/3 rounded-lg border border-gray-300 p-2"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          aria-label="Search Training Menu"
+        />
+
+        <label htmlFor="sort" className="sr-only">
+          Sort By
+        </label>
+        <select
+          id="sort"
+          className="rounded-lg border border-gray-300 p-2"
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+          aria-label="Sort Alphabetically"
+        >
+          <option value="asc">A - Z</option>
+          <option value="desc">Z - A</option>
+        </select>
+      </div>
+      <MenuDiv filteredMenu={filteredMenu} />
+    </div>
+  );
+};
+
+export default TrainingPage;
