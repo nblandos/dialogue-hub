e5647e0519d8c2c04599b0fe78479cd3815788d9
nblandos <nblandos@gmail.com>
2025-02-01 13:36:20 +0000

    fix dyslexic font import by using fontsource/opendyslexic

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import '@fontsource/opendyslexic/400.css';
+import '@fontsource/opendyslexic/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
