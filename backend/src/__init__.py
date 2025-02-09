e0ff1851471e6f958fd4395dde5beb120628b705
nblandos <nblandos@gmail.com>
2025-02-09 16:22:02 +0000

    integrate openai route with flask backend, use azure key vault aswell

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,7 +1,9 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from .services.ai_service import AIService
 
 db = SQLAlchemy()
 migrate = Migrate()
 mail = Mail()
+ai_service = AIService()
