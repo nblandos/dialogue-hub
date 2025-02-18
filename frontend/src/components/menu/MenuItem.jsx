ffd55d33a4b6a2c3b64e201a01b11c80331e733e
nblandos <nblandos@gmail.com>
2025-02-18 16:39:41 +0000

    format menuitem

--- frontend/src/components/menu/MenuItem.jsx
+++ frontend/src/components/menu/MenuItem.jsx
@@ -26,26 +26,29 @@ const MenuItem = ({ name, price, video }) => {
 
   return (
     <div
-      className="rounded-lg bg-white p-2 shadow-lg transition-transform duration-300 hover:shadow-xl flex flex-col items-center"
+      className="flex flex-col items-center rounded-lg bg-white p-2 shadow-lg transition-transform duration-300 hover:shadow-xl"
       style={{
         transform: hoveredVideo === name ? 'scale(1.05)' : 'scale(1)', // Scale entire container
       }}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <h2 className="mb-2 text-center text-lg font-semibold" tabIndex="0" data-screen-reader-text={name} >
-        {name} {price && <span className="text-sm text-gray-500">({price})</span>}
+      <h2
+        className="mb-2 text-center text-lg font-semibold"
+        tabIndex="0"
+        data-screen-reader-text={name}
+      >
+        {name}{' '}
+        {price && <span className="text-sm text-gray-500">({price})</span>}
       </h2>
 
-      <div
-        className="relative w-[90vw] sm:w-[45vw] md:w-[30vw] lg:w-[20vw] xl:w-[15vw] aspect-[9/16] flex justify-center items-center rounded-lg"
-      >
+      <div className="relative flex aspect-[9/16] w-[90vw] items-center justify-center rounded-lg sm:w-[45vw] md:w-[30vw] lg:w-[20vw] xl:w-[15vw]">
         <iframe
           loading="lazy"
           ref={(el) => (videoRefs.current[name] = el)}
           src={video}
           title={name}
-          className="w-full h-full rounded-lg shadow-md transition-transform duration-300"
+          className="h-full w-full rounded-lg shadow-md transition-transform duration-300"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
