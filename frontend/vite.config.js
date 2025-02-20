8e4893b1ca92b431754ce75f91467d044ffe89a4
nblandos <nblandos@gmail.com>
2025-02-20 00:38:28 +0000

    fix env var paths

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5001',
+        target: import.meta.env.VITE_API_URL || 'http://localhost:5001',
         changeOrigin: true,
         secure: true,
       },
