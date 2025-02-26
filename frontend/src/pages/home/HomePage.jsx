621ec96d2b62f90a811243e7b59a6323121e3f8e
nblandos <nblandos@gmail.com>
2025-02-26 13:54:25 +0000

    format home page

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -36,13 +36,19 @@ const HomePage = () => {
     <div className="flex flex-col items-center bg-gray-100 p-6 pt-32 lg:flex-row lg:items-start">
       <div className="flex w-full flex-col items-center rounded-lg border-gray-300 bg-white p-4 text-center lg:w-3/4">
         <div className="mb-4 mt-2 w-4/5 text-center">
-          <h1 className="mb-2 text-4xl font-bold text-gray-900" data-screen-reader-text="Dialogue Cafe">
+          <h1
+            className="mb-2 text-4xl font-bold text-gray-900"
+            data-screen-reader-text="Dialogue Cafe"
+          >
             Dialogue Cafe
           </h1>
-          <p className="mb-6 text-xl text-gray-700" data-screen-reader-text="Dialogue Cafe has a very strong commitment to social inclusion and
+          <p
+            className="mb-6 text-xl text-gray-700"
+            data-screen-reader-text="Dialogue Cafe has a very strong commitment to social inclusion and
             change. We encourage our customers to order in British Sign Language
             to spark a conversation between visitors and our deaf or hard of
-            hearing baristas.">
+            hearing baristas."
+          >
             Dialogue Cafe has a very strong commitment to social inclusion and
             change. We encourage our customers to order in British Sign Language
             to spark a conversation between visitors and our deaf or hard of
@@ -53,17 +59,22 @@ const HomePage = () => {
         {/* Divider */}
         <div className="mb-8 w-full border-b border-gray-300"></div>
 
-        <header className="mb-4 text-3xl font-bold" data-screen-reader-text="Popular Orders">Popular Orders</header>
+        <header
+          className="mb-4 text-3xl font-bold"
+          data-screen-reader-text="Popular Orders"
+        >
+          Popular Orders
+        </header>
 
         {/* Popular Orders Videos Grid */}
         <div className="mb-8 grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
           {popularOrders.map((order) => (
             <div
-                  key={order.name}
-                  className="flex flex-col items-center text-center"
-                  onMouseEnter={() => handleMouseEnter(order.name)}
-                  onMouseLeave={() => handleMouseLeave(order.name)}
-                  data-screen-reader-text={order.name}
+              key={order.name}
+              className="flex flex-col items-center text-center"
+              onMouseEnter={() => handleMouseEnter(order.name)}
+              onMouseLeave={() => handleMouseLeave(order.name)}
+              data-screen-reader-text={order.name}
             >
               <p className="mb-2 text-xl font-semibold">{order.name}</p>
               <iframe
@@ -106,7 +117,9 @@ const HomePage = () => {
             className="flex items-center space-x-2 rounded-lg bg-purple-600 px-6 py-3 text-white shadow-md transition-all duration-200 hover:scale-105 hover:bg-purple-700"
           >
             <FaGraduationCap className="text-xl" />
-            <span data-screen-reader-text="Learn Sign Language">Learn Sign Language</span>
+            <span data-screen-reader-text="Learn Sign Language">
+              Learn Sign Language
+            </span>
           </a>
         </div>
 
@@ -115,7 +128,12 @@ const HomePage = () => {
 
         {/* Tube Map Image*/}
         <div className="mt-4 flex w-full flex-col items-center space-y-2">
-          <h2 className="text-3xl font-bold" data-screen-reader-text="Getting Here">Getting Here</h2>
+          <h2
+            className="text-3xl font-bold"
+            data-screen-reader-text="Getting Here"
+          >
+            Getting Here
+          </h2>
           <div className="flex w-full items-start justify-between gap-8 px-4">
             <div className="w-2/3">
               <img
@@ -127,26 +145,67 @@ const HomePage = () => {
             <div className="w-1/3 space-y-6 text-left">
               <div className="space-y-2">
                 <h3 className="text-xl font-semibold">Address</h3>
-                <p className="text-gray-700" data-screen-reader-text="Royal Docks Center for Sustainability">
+                <p
+                  className="text-gray-700"
+                  data-screen-reader-text="Royal Docks Center for Sustainability"
+                >
                   Royal Docks Center for Sustainability
                 </p>
-                <p className="text-gray-700" data-screen-reader-text=" University of East London Docklands Campus">
+                <p
+                  className="text-gray-700"
+                  data-screen-reader-text=" University of East London Docklands Campus"
+                >
                   University of East London Docklands Campus
                 </p>
-                <p className="text-gray-700" data-screen-reader-text="4-6 University Way">4-6 University Way</p>
-                <p className="text-gray-700" data-screen-reader-text="London E16 2RD">London E16 2RD</p>
+                <p
+                  className="text-gray-700"
+                  data-screen-reader-text="4-6 University Way"
+                >
+                  4-6 University Way
+                </p>
+                <p
+                  className="text-gray-700"
+                  data-screen-reader-text="London E16 2RD"
+                >
+                  London E16 2RD
+                </p>
               </div>
 
               <div className="space-y-4">
-                <h3 className="text-xl font-semibold" data-screen-reader-text="Opening Hours">Opening Hours</h3>
+                <h3
+                  className="text-xl font-semibold"
+                  data-screen-reader-text="Opening Hours"
+                >
+                  Opening Hours
+                </h3>
                 <div className="space-y-2">
                   <div className="flex justify-between">
-                    <span className="text-gray-700" data-screen-reader-text="Monday-Thursday">Monday-Thursday</span>
-                    <span className="text-gray-700" data-screen-reader-text="08:00-17:00">08:00-17:00</span>
+                    <span
+                      className="text-gray-700"
+                      data-screen-reader-text="Monday-Thursday"
+                    >
+                      Monday-Thursday
+                    </span>
+                    <span
+                      className="text-gray-700"
+                      data-screen-reader-text="08:00-17:00"
+                    >
+                      08:00-17:00
+                    </span>
                   </div>
                   <div className="flex justify-between">
-                    <span className="text-gray-700" data-screen-reader-text="Friday">Friday</span>
-                    <span className="text-gray-700" data-screen-reader-text="08:00-13:00">08:00-13:00</span>
+                    <span
+                      className="text-gray-700"
+                      data-screen-reader-text="Friday"
+                    >
+                      Friday
+                    </span>
+                    <span
+                      className="text-gray-700"
+                      data-screen-reader-text="08:00-13:00"
+                    >
+                      08:00-13:00
+                    </span>
                   </div>
                 </div>
               </div>
@@ -157,7 +216,12 @@ const HomePage = () => {
 
       {/* Order Example Column */}
       <div className="mt-6 flex w-full flex-col items-center lg:mt-0 lg:w-1/4">
-        <h1 className="mb-6 text-3xl font-bold" data-screen-reader-text="How to Order">How to Order</h1>
+        <h1
+          className="mb-6 text-3xl font-bold"
+          data-screen-reader-text="How to Order"
+        >
+          How to Order
+        </h1>
         <div className="flex flex-col items-center space-y-6">
           {[
             { name: 'Hello-Please-Thank You' },
@@ -165,12 +229,11 @@ const HomePage = () => {
             { name: 'Goodbye' },
           ].map((item, index) => (
             <React.Fragment key={item.name}>
-                  <div
-                      
+              <div
                 className="flex flex-col items-center text-center"
                 onMouseEnter={() => handleMouseEnter(item.name)}
-                      onMouseLeave={() => handleMouseLeave(item.name)}
-                      data-screen-reader-text={item.name}
+                onMouseLeave={() => handleMouseLeave(item.name)}
+                data-screen-reader-text={item.name}
               >
                 <p className="mb-4 text-xl font-semibold">{item.name}</p>
                 <iframe
