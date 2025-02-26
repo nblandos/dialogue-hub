e5fa17af99128a3c35d79615dc70ae0e0eeb3d72
nblandos <nblandos@gmail.com>
2025-02-26 15:01:02 +0000

    responsive MenuItem and mute by default

--- frontend/src/components/menu/MenuItem.jsx
+++ frontend/src/components/menu/MenuItem.jsx
@@ -1,8 +1,12 @@
-import React, { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 const MenuItem = ({ name, price, video }) => {
   const [hoveredVideo, setHoveredVideo] = useState(null);
   const videoRefs = useRef({});
+
+  const getVideoUrl = (videoUrl) => {
+    return `${videoUrl}&mute=1`;
+  };
 
   const handleMouseEnter = () => {
     setHoveredVideo(name);
@@ -42,11 +46,11 @@ const MenuItem = ({ name, price, video }) => {
         {price && <span className="text-sm text-gray-500">({price})</span>}
       </h2>
 
-      <div className="relative flex aspect-[9/16] w-[90vw] items-center justify-center rounded-lg sm:w-[45vw] md:w-[30vw] lg:w-[20vw] xl:w-[15vw]">
+      <div className="relative flex aspect-[9/16] w-full max-w-[350px] items-center justify-center rounded-lg sm:max-w-[360px] md:max-w-[300px] lg:max-w-[250px] xl:max-w-[280px]">
         <iframe
           loading="lazy"
           ref={(el) => (videoRefs.current[name] = el)}
-          src={video}
+          src={getVideoUrl(video)}
           title={name}
           className="h-full w-full rounded-lg shadow-md transition-transform duration-300"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
